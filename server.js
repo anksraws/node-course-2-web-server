064e5bfcdec87b1775a0c77f756e17a86702b6ed
Pooha <poojarawat@Poohas-MacBook-Pro.local>
2019-01-22 21:10:51 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,89 @@
+ const express = require('express');
+ const hbs = require('hbs');
+ const fs = require('fs');
+ var app = express();
+
+ hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine','hbs');
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+     console.log(log);
+     
+     fs.appendFile('server.log', log +'\n', (err) => {
+       if(err) {
+       	console.log('Unable to append to server.log');
+       }
+     });
+	
+	next();
+
+});
+
+app.use((req, res, next) => {
+
+res.render('maintainance.hbs');
+
+});
+
+app.use(express.static(__dirname +'/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+
+return new Date().getFullYear();
+
+});
+
+hbs.registerHelper('screamIt' ,(text) => {
+return "hello";
+});
+
+ app.get('/', (req, res) => {
+
+//res.send('<h1>hello express!</h1>');
+res.send({
+	name: 'ankita',
+	likes: [
+	'singing',
+	'dancing'
+	]
+});
+
+ });
+
+ app.get('/route', (req, res) => {
+
+//res.send('<h1>hello express!</h1>');
+res.render('home.hbs',{
+	name: 'ankita',
+	likes: ['Singing',
+	'dancing']
+});
+
+ });
+
+app.get('/about', (req,res) => {
+
+res.render('about.hbs', {
+	pageTitle: 'About Page',
+	
+});
+
+});
+
+app.get('/bad', (req,res) => {
+
+   res.send({
+
+   	errorMsg: 'unable to handle',
+
+   });
+
+});
+
+ app.listen(3000, () => {
+ 	console.log('server is running in port 3000');
+ });//binding setup
